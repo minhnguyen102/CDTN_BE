db63d143654ccb8e5c3d1ffba3814cc28289a9dd
Minh Nguyen <minhkhac1002@gmail.com>
2025-10-22 22:24:18 +0700

    Kết nối db

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express, { Router } from "express"
 import { routesAdmin } from "./routes/admins/index.routes"
+import { run } from "~/services/database.servies"
 const app = express()
 const port = 3000
 
+run().catch(console.dir)
+
+// Router admin
 routesAdmin(app)
 
 app.listen(port, () => {
---
+++ src/services/database.servies.ts
@@ -0,0 +1,20 @@
+import { MongoClient } from "mongodb"
+import { config } from "dotenv"
+config()
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.sqjfe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri)
+
+export async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect()
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 })
+    console.log("Pinged your deployment. You successfully connected to MongoDB!")
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close()
+  }
+}
