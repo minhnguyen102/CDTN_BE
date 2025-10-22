84cd34a0fc618a102032ea013a63a2f6f043239e
Minh Nguyen <minhkhac1002@gmail.com>
2025-10-22 22:31:48 +0700

    Cấu trúc lại db theo Class

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express, { Router } from "express"
 import { routesAdmin } from "./routes/admins/index.routes"
-import { run } from "~/services/database.servies"
+import databaseService from "~/services/database.servies"
 const app = express()
 const port = 3000
 
-run().catch(console.dir)
+// Connect database
+databaseService.connect().catch(console.dir)
 
 // Router admin
 routesAdmin(app)
--- src/services/database.servies.ts
+++ src/services/database.servies.ts
@@ -1,20 +1,29 @@
-import { MongoClient } from "mongodb"
+import { Db, MongoClient } from "mongodb"
 import { config } from "dotenv"
 config()
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.sqjfe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri)
+class DatabaseService {
+  private client: MongoClient
+  private db: Db
+  constructor() {
+    this.client = new MongoClient(uri)
+    this.db = this.client.db(process.env.DB_NAME)
+  }
 
-export async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect()
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 })
-    console.log("Pinged your deployment. You successfully connected to MongoDB!")
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close()
+  async connect() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await this.client.connect()
+      // Send a ping to confirm a successful connection
+      await this.db.command({ ping: 1 })
+      console.log("Pinged your deployment. You successfully connected to MongoDB!")
+    } catch (err) {
+      console.log(err)
+      throw err
+    }
   }
 }
+
+const databaseService = new DatabaseService()
+export default databaseService
