a52bf062cb4b153844aba648b9e9e06cf66a0d06
Minh Nguyen <minhkhac1002@gmail.com>
2025-11-01 21:31:46 +0700

    Configure for Vercel deployment

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,9 @@ routesAdmin(app)
 
 app.use(defaultErrorHandler)
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+// Deploy
+export default app
