d402d2a6980269db475b03069dad7240e1daf6ec
Minh Nguyen <minhkhac1002@gmail.com>
2025-11-05 11:49:11 +0700

    import cors

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,18 @@ import express from "express"
 import { routesAdmin } from "./routes/admins/index.routes"
 import databaseService from "./services/database.servies"
 import { defaultErrorHandler } from "./middlewares/error.middlewares"
-import QRCode, { QRCodeSegment } from "qrcode"
+import cors from "cors"
 const app = express()
 const port = 3000
 
 app.use(express.json())
+
+//cors
+const corsOptions = {
+  origin: "http://localhost:3000",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
+}
+app.use(cors(corsOptions))
 
 // Connect database
 databaseService.connect().catch(console.dir)
@@ -15,26 +22,6 @@ databaseService.connect().catch(console.dir)
 routesAdmin(app)
 
 app.use(defaultErrorHandler)
-app.get("/", (req, res) => {
-  res.send("API Backend (CDTN_BE) đang chạy!")
-})
-
-const websiteURL = "https://www.facebook.com/"
-app.get("/qr-link", async (req, res) => {
-  try {
-    // 1. Mã hóa URL thành ảnh QR (dạng Data URL)
-    const qrCodeImageURL = await QRCode.toDataURL(websiteURL)
-    console.log("qrCodeImageURL", qrCodeImageURL)
-
-    // 2. Gửi ảnh QR về trình duyệt để bạn xem
-    res.send(`
-      <h1>Quét mã này để mở trang web</h1>
-      <img src="${qrCodeImageURL}" alt="QR Code">
-    `)
-  } catch (err) {
-    res.status(500).send("Lỗi tạo QR")
-  }
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
