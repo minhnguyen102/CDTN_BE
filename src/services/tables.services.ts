5d91aea5fff69a1972b689443727da5f2d5a6f04
Minh Nguyen <minhkhac1002@gmail.com>
2025-11-05 15:08:03 +0700

    get all table and pagination

--- src/services/tables.services.ts
+++ src/services/tables.services.ts
@@ -1,9 +1,9 @@
 import databaseService from "./database.servies"
 import Table from "../models/schema/Table.schema"
-import { randomBytes } from "crypto"
 import { TableStatus } from "../constants/enums"
 import { genQRtable } from "../utils/qr"
 import { randomQrToken } from "../utils/crypto"
+import { paginationHelper } from "../utils/helpers"
 import { table } from "console"
 
 class TableServices {
@@ -38,18 +38,56 @@ class TableServices {
     }
   }
 
-  async viewTableController() {
+  async getAllTablesController({ page }: { page: number }) {
+    const objectPagination: {
+      currentPage: number
+      skip?: number
+      limit: number
+      totalPage?: number
+    } = {
+      currentPage: page,
+      limit: 2
+    }
+    const totalDocument = await databaseService.tables.countDocuments()
+    const newObjectPagination = paginationHelper({
+      totalDocument,
+      objectPagination
+    })
+    // console.log("newObjectPagination: ", newObjectPagination)
+    // console.log("objectPagination: ", objectPagination)
     const tables = await databaseService.tables
-      .find(
-        {},
-        {
-          projection: {
-            qrToken: 0
-          }
-        }
-      )
+      .find()
+      .limit(objectPagination.limit)
+      .skip(objectPagination.skip as number)
       .toArray()
-    return tables
+
+    const qrGenerationPromises = tables.map(async (table) => {
+      // a. Tạo link URL từ qrToken (token bí mật)
+      const qrToken = table.qrToken
+
+      // b. Tạo ảnh QR (dạng Data URL)
+      const qrCodeImage = await genQRtable({ qrToken })
+
+      // c. Trả về một OBJECT MỚI.
+      //    **QUAN TRỌNG**: Không được trả về qrToken (chuỗi bí mật) cho client!
+      return {
+        _id: table._id,
+        number: table.number,
+        capacity: table.capacity,
+        status: table.status,
+        // (Thêm các trường khác bạn cần hiển thị...)
+
+        QRcode: qrCodeImage // Thêm thuộc tính mới là ảnh QR
+      }
+    })
+
+    // 3. Đợi TẤT CẢ các "lời hứa" (hàm tạo QR) chạy xong
+    const tablesWithQR = await Promise.all(qrGenerationPromises)
+    // console.log(tablesWithQR)
+    return {
+      tables: tablesWithQR,
+      ...objectPagination
+    }
   }
 }
 
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,29 @@
-import { query } from "express-validator"
+import { Request } from "express"
 
-export function paginationHelper() {}
+/**
+ *  Tham số nhận vào (1 object có dạng định trả về, totalDocument)
+ *
+ * Mục đích của pagination helper => Trả về object {
+ *  currentPage: 2
+ *  limitItem: 10 // cố định
+ *  skip: Sau tính toán (= (currentPage -1) * limitItem)
+ *  totalPage: Sau tính toán (= Math.celi(totalDocument / limitItem)) <= totalPage : count db
+ * }
+ */
+interface objectPaginationType {
+  currentPage: number
+  skip?: number
+  limit: number
+  totalPage?: number
+}
+export function paginationHelper({
+  totalDocument,
+  objectPagination
+}: {
+  totalDocument: number
+  objectPagination: objectPaginationType
+}) {
+  objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limit
+  objectPagination.totalPage = Math.ceil(totalDocument / objectPagination.limit)
+  return objectPagination
+}
