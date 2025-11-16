88da87b958e3e315550f626af14b22f174e00b1f
Minh Nguyen <minhkhac1002@gmail.com>
2025-11-16 21:03:47 +0700

    pagination, filter and search supplier

--- src/controllers/admins/suppliers.controllers.ts
+++ src/controllers/admins/suppliers.controllers.ts
@@ -5,9 +5,17 @@ import supplierServices from "../../services/suppliers.services"
 import USER_MESSAGES from "../../constants/message"
 import { pick } from "lodash"
 import HTTP_STATUS from "../../constants/httpStatus"
+import { paginationQueryParser } from "../../utils/helpers"
 
 export const viewAllSupplierController = async (req: Request, res: Response) => {
-  const result = await supplierServices.getAllSuppliers()
+  // pagination
+  const { page, limit } = paginationQueryParser(req, { defaultLimit: 5, allowLimits: [5, 10, 15] })
+  // Xử lí status
+  const status = (req.query.status as string) || undefined
+  // Xử lí search
+  const search = (req.query.search as string) || undefined
+
+  const result = await supplierServices.getAllSuppliers({ page, limit, status, search })
   res.json({
     message: USER_MESSAGES.GET_ALL_SUPPLIERS_SUCCESS,
     result
--- src/services/suppliers.services.ts
+++ src/services/suppliers.services.ts
@@ -5,24 +5,77 @@ import { ObjectId } from "mongodb"
 import { ErrorWithStatus } from "../models/Errors"
 import USER_MESSAGES from "../constants/message"
 import HTTP_STATUS from "../constants/httpStatus"
+import { SupplierStatus } from "../constants/enums"
 
 class SupplierServices {
-  async getAllSuppliers() {
-    // Cần thiết sẽ mở rộng phân trang
-    const objectFind = {
+  async getAllSuppliers({
+    page,
+    limit,
+    status,
+    search
+  }: {
+    page: number
+    limit: number
+    status?: string
+    search?: string
+  }) {
+    const objectFind: any = {
       isDeleted: false
     }
-    const result = await databaseService.suppliers
-      .find(objectFind, {
-        projection: {
-          createdAt: 0,
-          updatedAt: 0,
-          isDeleted: 0,
-          deletedAt: 0
-        }
-      })
-      .toArray()
-    return result
+    // FilterStatus
+    if (status) {
+      const validStatuses = Object.values(SupplierStatus) as string[]
+      if (validStatuses.includes(status)) {
+        objectFind.status = status as SupplierStatus
+      } else {
+        throw new ErrorWithStatus({
+          message: `Trạng thái filter không hợp lệ`,
+          status: HTTP_STATUS.BAD_REQUEST
+        })
+      }
+    }
+    if (search) {
+      // const searchRegex = { $regex: search, $options: "i" } // 'i' = không phân biệt hoa thường
+      // console.log(searchRegex)
+      // objectFind.$or = [
+      //   { name: searchRegex },
+      //   { phone: searchRegex },
+      //   { email: searchRegex },
+      //   { contactPerson: searchRegex }
+      // ]
+      // console.log(search)
+      objectFind.$text = { $search: search }
+    }
+
+    const skip = (page - 1) * limit
+
+    const [suppliers, totalFilteredDocuments] = await Promise.all([
+      databaseService.suppliers
+        .find(objectFind, {
+          projection: {
+            createdAt: 0,
+            updatedAt: 0,
+            isDeleted: 0,
+            deletedAt: 0
+          }
+        })
+        .limit(limit)
+        .skip(skip)
+        .toArray(),
+      databaseService.suppliers.countDocuments(objectFind)
+    ])
+    // Tính toán totalPage (dựa trên tổng đã lọc)
+    const totalPage = Math.ceil(totalFilteredDocuments / limit)
+
+    return {
+      suppliers,
+      pagination: {
+        currentPage: page,
+        limit: limit,
+        total: totalFilteredDocuments, // Tổng số item (đã lọc)
+        totalPage: totalPage // Tổng số trang (đã lọc)
+      }
+    }
   }
 
   async createSupplier({ payload }: { payload: createSupplierReqBody }) {
