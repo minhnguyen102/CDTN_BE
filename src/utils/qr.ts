513b7e75a4e3cd778059868a8361d0cb1f39fde6
Minh Nguyen <minhkhac1002@gmail.com>
2025-11-28 15:35:31 +0700

    update url table when scan qr

--- src/utils/qr.ts
+++ src/utils/qr.ts
@@ -1,6 +1,6 @@
 import QRCode from "qrcode" // <-- 1. BẠN IMPORT THƯ VIỆN Ở ĐÂY
 export async function genQRtable({ qrToken }: { qrToken: string }) {
-  const orderUrl = `https://your-app-frontend.com/order?token=${qrToken}`
+  const orderUrl = `${process.env.BASE_URL}/guest?token=${qrToken}`
   const qrCodeImageString = await QRCode.toDataURL(orderUrl, {
     errorCorrectionLevel: "H"
   })
