9aed92e081c00c2dc4d1f8044b585a450d39b747
Minh Nguyen <minhkhac1002@gmail.com>
2025-12-01 14:25:29 +0700

    update evn p2

--- src/utils/qr.ts
+++ src/utils/qr.ts
@@ -1,4 +1,4 @@
-import QRCode from "qrcode" //
+import QRCode from "qrcode"
 export async function genQRtable({ qrToken }: { qrToken: string }) {
   const orderUrl = `${process.env.BASE_URL}/guest?token=${qrToken}`
   const qrCodeImageString = await QRCode.toDataURL(orderUrl, {
