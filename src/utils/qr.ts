88c7f96f3d8d906c8ab8a9162cd684d09a6227ff
Minh Nguyen <minhkhac1002@gmail.com>
2025-12-01 14:20:02 +0700

    change env

--- src/utils/qr.ts
+++ src/utils/qr.ts
@@ -1,4 +1,4 @@
-import QRCode from "qrcode" // <-- 1. BẠN IMPORT THƯ VIỆN Ở ĐÂY
+import QRCode from "qrcode" //
 export async function genQRtable({ qrToken }: { qrToken: string }) {
   const orderUrl = `${process.env.BASE_URL}/guest?token=${qrToken}`
   const qrCodeImageString = await QRCode.toDataURL(orderUrl, {
