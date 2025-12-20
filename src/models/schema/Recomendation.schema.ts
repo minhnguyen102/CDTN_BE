489419ee7cb04d97da294b3400bca95548e60b21
Minh Nguyen <minhkhac1002@gmail.com>
2025-12-20 14:40:02 +0700

    Điều chỉnh collection Recomendation

--- src/models/schema/Recomendation.schema.ts
+++ src/models/schema/Recomendation.schema.ts
@@ -2,19 +2,20 @@ import { ObjectId } from "mongodb"
 
 interface RecommendationType {
   _id?: ObjectId
-  dishId: ObjectId
+  combinationKey?: string
   recommendedDishIds: string[]
   updatedAt: Date
 }
 
 export default class Recommendation {
   _id?: ObjectId
-  dishId: ObjectId
+  combinationKey: string
   recommendedDishIds: ObjectId[]
   updatedAt: Date
 
   constructor(recomentdation: RecommendationType) {
-    this.dishId = new ObjectId(recomentdation.dishId)
+    this._id = recomentdation._id
+    this.combinationKey = recomentdation.combinationKey || ""
     this.recommendedDishIds = recomentdation.recommendedDishIds.map((id: string) => new ObjectId(id))
     this.updatedAt = new Date()
   }
