0b82ea89d21f81a97498fcf3a408075706355c19
Minh Nguyen <minhkhac1002@gmail.com>
2025-12-20 19:40:11 +0700

    tối ưu quá trình gợi ý món

--- src/services/ai.services.ts
+++ src/services/ai.services.ts
@@ -3,6 +3,7 @@ import databaseService from "./database.servies"
 import { DishStatus } from "../constants/enums"
 import { GoogleGenerativeAI } from "@google/generative-ai"
 import { config } from "dotenv"
+import { reject } from "lodash"
 config()
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string)
@@ -44,88 +45,94 @@ class AIService {
     // Nếu không có => Tạo mới bản ghi + hỏi AI
     // Lấy tên món ăn
     const dishObjectIds = dishIds.map((dishId) => new ObjectId(dishId))
-    const cartItems = await databaseService.dishes
-      .find({ _id: { $in: dishObjectIds } })
-      .project({ name: 1 })
-      .toArray()
+
+    const [cartItems, minimizedMenu] = await Promise.all([
+      databaseService.dishes
+        .find({ _id: { $in: dishObjectIds } })
+        .project({ name: 1 })
+        .toArray(),
+      databaseService.dishes // Lấy menu rút gọn: Gồm các món ăn ngoại trừ các món có trong cart
+        .aggregate([
+          {
+            $match: {
+              _id: { $nin: dishObjectIds },
+              status: DishStatus.AVAILABLE
+            }
+          },
+          {
+            $sample: { size: 30 }
+          },
+          {
+            $lookup: {
+              from: "dish_categories",
+              localField: "categoryId",
+              foreignField: "_id",
+              as: "dishCategoriesInfo"
+            }
+          },
+          {
+            $unwind: {
+              path: "$dishCategoriesInfo",
+              preserveNullAndEmptyArrays: true
+            }
+          },
+          {
+            $project: {
+              _id: 1,
+              name: 1,
+              dishCategoryName: "$dishCategoriesInfo.name"
+            }
+          }
+        ])
+        .toArray()
+    ])
+
     const cartNames = cartItems.map((item) => item.name).join(", ")
 
-    // Lấy menu rút gọn: Gồm các món ăn ngoại trừ các món có trong cart
-    const minimizedMenu = await databaseService.dishes
-      .aggregate([
-        {
-          $match: {
-            _id: { $nin: dishObjectIds },
-            status: DishStatus.AVAILABLE
-          }
-        },
-        {
-          $lookup: {
-            from: "dish_categories",
-            localField: "categoryId",
-            foreignField: "_id",
-            as: "dishCategoriesInfo"
-          }
-        },
-        {
-          $unwind: {
-            path: "$dishCategoriesInfo",
-            preserveNullAndEmptyArrays: true
-          }
-        },
-        {
-          $project: {
-            _id: 1,
-            name: 1,
-            dishCategoryName: "$dishCategoriesInfo.name"
-          }
-        }
-      ])
-      .toArray()
-    // console.log(minimizedMenu)
-
     const prompt = `
-            Khách đang có các món này trong giỏ hàng: "${cartNames}".
-            Menu quán gồm: ${JSON.stringify(minimizedMenu)}.
-            
-            Yêu cầu:
-            1. Chọn ra 3 món trong Menu bổ sung tốt nhất cho các món trong giỏ (Ví dụ: Giỏ nhiều đồ khô thì gợi ý đồ uống/canh).
-            2. Output: CHỈ trả về JSON Array chứa 3 chuỗi _id. Ví dụ: ["id1", "id2"]. Không giải thích.
+            Giỏ hàng: "${cartNames}".
+            Menu: ${JSON.stringify(minimizedMenu)}.
+            Chọn 3 món từ Menu hợp nhất để ăn kèm.
+            Output JSON: ["id1", "id2", "id3"]
         `
     try {
       const model = genAI.getGenerativeModel({
-        model: "gemini-2.5-flash",
+        model: "gemini-2.5-flash-lite",
 
         generationConfig: {
-          temperature: 0.9,
-          topK: 40,
+          temperature: 0.3,
+          topK: 20,
           topP: 0.95,
-          maxOutputTokens: 2048,
+          maxOutputTokens: 100,
           responseMimeType: "application/json"
         }
       })
-      const result = await model.generateContent(prompt)
-      const text = result.response.text()
+      const aiCall = model.generateContent(prompt)
+      const timeOut = new Promise((_, reject) => setTimeout(() => reject(new Error("TIME OUT")), 2500))
 
-      const cleanText = text
+      const result: any = await Promise.race([aiCall, timeOut])
+      const cleanText = result.response
+        .text()
         .replace(/```json/g, "")
         .replace(/```/g, "")
         .trim()
       const recommendedIdsString = JSON.parse(cleanText)
       const recommendedObjectIds = recommendedIdsString.map((id: string) => new ObjectId(id))
 
-      await databaseService.recommendations.insertOne({
-        combinationKey: sortedIds,
-        recommendedDishIds: recommendedObjectIds,
-        updatedAt: new Date()
-      })
+      databaseService.recommendations
+        .insertOne({
+          combinationKey: sortedIds,
+          recommendedDishIds: recommendedObjectIds,
+          updatedAt: new Date()
+        })
+        .catch((err) => console.error("Lỗi lưu cache:", err))
 
       return databaseService.dishes
         .find({ _id: { $in: recommendedObjectIds } })
         .project({ name: 1, price: 1, image: 1, ratingAverage: 1, reviewCount: 1 })
         .toArray()
-    } catch (error) {
-      console.error("Lỗi AI Cart:", error)
+    } catch (error: any) {
+      if (error.message !== "TIMEOUT") console.error("Lỗi AI:", error)
       // Fallback: Trả về 3 món ngẫu nhiên nếu lỗi
       return databaseService.dishes
         .find(
